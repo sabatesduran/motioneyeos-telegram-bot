649017ceb9d5da8107b3ee4a4d7e4c90891f017b
Dídac Sabatés <didac@verse.me>
2020-04-12 14:19:27 +0200

    Improve list_of_admins

--- settings.py
+++ settings.py
@@ -4,7 +4,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-LIST_OF_ADMINS = json.loads(os.getenv('LIST_OF_ADMINS'))
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 MOTIONEYEOS_URL = os.getenv('MOTIONEYEOS_URL')
@@ -12,4 +11,5 @@ MOTIONEYEOS_USER =  os.getenv('MOTIONEYEOS_USER')
 MOTIONEYEOS_API_KEY =  os.getenv('MOTIONEYEOS_API_KEY')
 
 def get_list_of_admins():
-  return [int(admin) for admin in LIST_OF_ADMINS]
+  admins = json.loads(os.getenv('LIST_OF_ADMINS'))
+  return [int(admin) for admin in admins]
